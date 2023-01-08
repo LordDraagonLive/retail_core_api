7ff045fad3b1dc3b89a81afd4a56ccc760bb8ea6
Buddhi Adhikari <buddhiadikari@live.com>
2023-01-08 14:26:52 +0530

    Upgraded the project to nightly build - nightly build upgrade. - added required nightly dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api", routes![index])
 }
